6370e34dc90aa79a46d5f2fc4014210b666069ca
jonath92 <jonathanheard92@gmail.com>
2021-08-21 11:33:31 +0200

    add files

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+const { IconApplet } = imports.ui.applet
+
+interface Arguments {
+    orientation: imports.gi.St.Side,
+    panelHeight: number,
+    instanceId: number
+}
+
+export function main(args: Arguments): imports.ui.applet.Applet {
+
+    const {
+        orientation,
+        panelHeight,
+        instanceId
+    } = args
+
+    const myApplet = new IconApplet(orientation, panelHeight, instanceId)
+
+    myApplet.set_applet_icon_symbolic_name('computer')
+    myApplet.on_applet_clicked = () => global.log('applet clicked')
+
+    return myApplet
+}
